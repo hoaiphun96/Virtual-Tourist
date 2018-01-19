2742bc214b3afaff07a8ee05000b3100f7660a5c
Phuong Nguyen (Jamie) <phnguyen@knox.edu>
2018-01-19 09:55:53 -0600

    Update and rename from photoViewCell to Photo ViewCell

--- Virtual Tourist/View/PhotoViewCell.swift
+++ Virtual Tourist/View/PhotoViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class photoViewCell: UICollectionViewCell {
+class PhotoViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     var blurView: UIVisualEffectView?
